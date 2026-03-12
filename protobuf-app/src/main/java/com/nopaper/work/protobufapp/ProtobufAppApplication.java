18e1b09398f1925aa2606477fe924ee91cf82a18
Saikat Barman <118311969+nopaperwork@users.noreply.github.com>
2026-03-12 21:55:28 +0530

    first commit
    first commit

---
+++ protobuf-app/src/main/java/com/nopaper/work/protobufapp/ProtobufAppApplication.java
@@ -0,0 +1,13 @@
+package com.nopaper.work.protobufapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtobufAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtobufAppApplication.class, args);
+    }
+
+}
---
+++ protobuf-app/src/test/java/com/nopaper/work/protobufapp/ProtobufAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nopaper.work.protobufapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtobufAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
